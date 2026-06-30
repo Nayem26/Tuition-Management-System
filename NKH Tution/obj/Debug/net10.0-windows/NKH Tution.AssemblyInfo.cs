112bd02d769e9f7f8955db407a7e0ab46983543c
Nayem26 <nayemjannatul628@gmail.com>
2026-07-01 00:21:14 +0600

    Update Project

--- NKH Tution/obj/Debug/net10.0-windows/NKH Tution.AssemblyInfo.cs
+++ NKH Tution/obj/Debug/net10.0-windows/NKH Tution.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NKH Tution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82036db9eb5e09a20b79d31c65eaf2bd570cfedf")]
 [assembly: System.Reflection.AssemblyProductAttribute("NKH Tution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NKH Tution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
